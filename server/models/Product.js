3cdda8e0427aa07524b41d74af0cce88cb7c86f4
Raevin Dunson <raevin.dunson98@gmail.com>
2021-10-26 20:12:20 -0400

    schemas updates

---
+++ server/models/Product.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose;
+
+const prodSchema = new productSchema({
+
+});
+
+const Product = prodSchema;
+
+module.exports = Product;
